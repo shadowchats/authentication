68bc64c67bb4b9f0e15e9564bc834e371ee6b52c
One290 <lenya.dorovskoy@mail.ru>
2025-08-19 17:45:25 +0800

    Копирайт и лицензия.
    Signed-off-by: One290 <lenya.dorovskoy@mail.ru>

--- Shadowchats.Authentication.Core/Domain/Entities/Session.cs
+++ Shadowchats.Authentication.Core/Domain/Entities/Session.cs
@@ -1,5 +1,10 @@
-﻿// Shadowchats - Copyright (C) 2025 Доровской Алексей Васильевич
-// Licensed under AGPL v3.0 - see file LICENSE
+﻿// Shadowchats — Copyright (C) 2025
+// Dorovskoy Alexey Vasilievich (One290 / 0ne290) <lenya.dorovskoy@mail.ru>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. See the LICENSE file for details.
+// For full copyright and authorship information, see the COPYRIGHT file.
 
 namespace Shadowchats.Authentication.Core.Domain.Entities;
 
