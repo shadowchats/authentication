195f7aa41bd70e2667928afa193484fddb6df0d4
One290 <lenya.dorovskoy@mail.ru>
2025-08-31 19:59:17 +0800

    .
    Signed-off-by: One290 <lenya.dorovskoy@mail.ru>

--- Shadowchats.Authentication.Infrastructure/Persistence/AuthenticationDbContextFactory.cs
+++ Shadowchats.Authentication.Infrastructure/Persistence/AuthenticationDbContextFactory.cs
@@ -1,16 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+﻿using Microsoft.EntityFrameworkCore.Design;
 
 namespace Shadowchats.Authentication.Infrastructure.Persistence;
 
 public class AuthenticationDbContextFactory : IDesignTimeDbContextFactory<AuthenticationDbContext>
 {
-    public AuthenticationDbContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<AuthenticationDbContext>();
-
-        optionsBuilder.UseNpgsql(args[0]);
-        
-        return new AuthenticationDbContext(optionsBuilder.Options);
-    }
+    public AuthenticationDbContext CreateDbContext(string[] args) => new(args[0]);
 }
